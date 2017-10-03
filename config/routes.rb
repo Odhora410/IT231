288597f30c9db81e63c2d5419888be0b47ea5999
Omar Dhorajiwala <omardhorajiwala410@gmail.com>
2017-10-03 20:54:46 +0000

    my pages aqua

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  get 'my_pages/home'
+
+  get 'my_pages/help'
+
+  get 'my_pages/about'
+
+  resources :users
+  root 'users#index'
  end
